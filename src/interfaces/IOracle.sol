d343eee4fc217dff4c6d7f6366305de4dd0faf20
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-12-31 22:04:14 +0800

    update IOracle

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-
 interface IOracle {
     function getOutcomeTokenIds(bytes32 marketIdentifier) external pure returns (uint,uint);
     function getReserveTokenIds(bytes32 marketIdentifier) external pure returns (uint,uint);
