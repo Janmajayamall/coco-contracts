488812714b145e1d8f94139ee20bfcb98571eb53
Janamajaya Mall <janmajaya.choithram@gmail.com>
2022-01-18 18:50:44 +0800

    added proxies

--- src/proxies/OracleProxyFactory.sol
+++ src/proxies/OracleProxyFactory.sol
@@ -3,13 +3,70 @@
 pragma solidity ^0.8.0;
 
 import "./OracleProxy.sol";
+import "./../../lib/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
 
 contract OracleProxyFactory {
 
-    function createProxy(address _singleton, bytes memory data) external returns (OracleProxy _proxy) {
-        _proxy = new OracleProxy(_singleton);
+    function createOracle(
+        address _oracleSingleton,
+        address _safeSingleton,        
+        address _tokenC, 
+        bool _isActive, 
+        uint32 _feeNumerator, 
+        uint32 _feeDenominator,
+        uint16 _donEscalationLimit, 
+        uint32 _expireBufferBlocks, 
+        uint32 _donBufferBlocks, 
+        uint32 _resolutionBufferBlocks,
+        address[] memory owners,
+        uint256 threshold
+    ) external returns (OracleProxy _proxy) {
+        // deploy gnosis safe
+        GnosisSafeProxy _safeProxy = new GnosisSafeProxy(_safeSingleton);
+        
+        // setup safe
+        bytes memory safeSetupCall = abi.encodeWithSignature(
+                "setup(address[] calldata,uint256,address,bytes calldata,address,address,uint256,address payable)",
+                owners,
+                threshold,
+                address(0),
+                0,
+                0,
+                0,
+                0,
+                0
+            );
         assembly {
-            if eq(call(gas(), _proxy, 0,add(data, 0x20), mload(data), 0, 0), 0) {
+            if eq(call(gas(), _safeProxy, 0, add(safeSetupCall, 0x20), mload(safeSetupCall), 0, 0), 0) {
+                revert(0, 0)
+            }
+        }
+
+
+        OracleProxy _oracleProxy = new OracleProxy(_oracleSingleton);
+
+        // setup oracle
+        bytes memory updateManagerCall = abi.encodeWithSignature(
+                "updateManager(address)",
+                address(_safeProxy)
+            );
+        assembly {
+            if eq(call(gas(), _oracleProxy, 0, add(updateManagerCall, 0x20), mload(updateManagerCall), 0, 0), 0) {
+                revert(0, 0)
+            }
+        }
+        bytes memory updateMarketConfigCall = abi.encodeWithSignature(
+                "updateMarketConfig(bool,uint32,uint32,uint16,uint32,uint32,uint32)",
+                _isActive, 
+                _feeNumerator, 
+                _feeDenominator,
+                _donEscalationLimit, 
+                _expireBufferBlocks, 
+                _donBufferBlocks, 
+                _resolutionBufferBlocks
+            );
+        assembly {
+            if eq(call(gas(), _oracleProxy, 0, add(updateMarketConfigCall, 0x20), mload(updateMarketConfigCall), 0, 0), 0) {
                 revert(0, 0)
             }
         }
